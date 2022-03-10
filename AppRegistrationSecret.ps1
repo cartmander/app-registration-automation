1ec703c8ea3008ab6609d5fbf3a3efa9598b689d
KEVIN3349 <KEVIN3349@PCM-70KFP13>
2022-03-10 18:46:35 +0800

    Finalized secret generation for app registration including key vault upload

---
+++ AppRegistrationSecret.ps1
@@ -0,0 +1,55 @@
+param(
+    [string[]] $appRegistrationNames,
+    [string] $keyVaultName,
+    [int] $duration
+)
+
+function SecretDuration
+{
+    if ($duration -eq $null -or $duration -lt 1)
+    {
+        $duration = 1
+    }
+
+    return $duration
+}
+
+function GenerateSecretForAppRegistration
+{
+    $duration = SecretDuration
+    $AADCertificate = az ad app credential reset --id $appId --years $duration | ConvertFrom-Json
+
+    $secret = az keyvault secret set --name $name --vault-name $keyVaultName --value $AADCertificate.password | ConvertFrom-Json
+
+    return $secret
+}
+
+function UploadSecretToKeyVault
+{
+    param(
+        [object] $secret
+    )
+
+    $createdDate = (Get-Date).ToUniversalTime()
+    $expiryDate = $createdDate.AddYears($duration).ToUniversalTime()
+
+    $setSecretCreatedDate = $createdDate.ToString("yyyy-MM-dd'T'HH:mm:ssZ")
+    $setSecretExpiryDate = $expiryDate.ToString("yyyy-MM-dd'T'HH:mm:ssZ")
+        
+    az keyvault secret set-attributes --id $secret.id --not-before $setSecretCreatedDate --expires $setSecretExpiryDate
+}
+
+foreach ($name in $appRegistrationNames)
+{
+    $getAADApplication = Get-AzureADApplication -Filter "DisplayName eq '$name'"
+
+    if ($getAADApplication -ne $null)
+    {    
+        $appId = $getAADApplication.AppId
+        $objectId = $getAADApplication.ObjectId
+        
+        $secret = GenerateSecretForAppRegistration
+        
+        UploadSecretToKeyVault $secret
+    }
+}
