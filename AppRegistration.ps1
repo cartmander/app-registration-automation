e1628cfb8cbe4df2abb0d4a7a29b37d6733f2353
KEVIN3349 <KEVIN3349@PCM-70KFP13>
2022-03-10 19:21:13 +0800

    Applied some changes on azure login

--- AppRegistration.ps1
+++ AppRegistration.ps1
@@ -9,8 +9,8 @@ function AzureLogin
     $context = [Microsoft.Azure.Commands.Common.Authentication.Abstractions.AzureRmProfileProvider]::Instance.Profile.DefaultContext
     $graphToken = [Microsoft.Azure.Commands.Common.Authentication.AzureSession]::Instance.AuthenticationFactory.Authenticate($context.Account, $context.Environment, $context.Tenant.Id.ToString(), $null, [Microsoft.Azure.Commands.Common.Authentication.ShowDialog]::Never, $null, "https://graph.microsoft.com").AccessToken
     $aadToken = [Microsoft.Azure.Commands.Common.Authentication.AzureSession]::Instance.AuthenticationFactory.Authenticate($context.Account, $context.Environment, $context.Tenant.Id.ToString(), $null, [Microsoft.Azure.Commands.Common.Authentication.ShowDialog]::Never, $null, "https://graph.windows.net").AccessToken
-    Write-Output "Hi I'm $($context.Account.Id)"
-    Connect-AzureAD -AadAccessToken $aadToken -AccountId $context.Account.Id -TenantId $context.tenant.id -MsAccessToken $graphToken
+    Write-Output "Hi I'm $($context)"
+    Connect-AzureAD -AadAccessToken $aadToken -AccountId $context.Account.Id -TenantId $context.Tenant.id -MsAccessToken $graphToken
 }
 
 function AddOwners
